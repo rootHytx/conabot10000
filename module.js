f6c88af4d9dda5a35ca3c12866d5ded2dabcc59a
hytx <r.dinisnaruto@gmail.com>
2020-11-25 11:08:11 +0000

    Add files via upload

---
+++ module.js
@@ -0,0 +1,23 @@
+var dir = 'D:/Downloads/PROGRAMAS/DiscordBot/conabot10000'; //define working environment
+let priestsarr = [
+	'https://cruxnow.com/wp-content/uploads/2019/10/12c068ae215bc03e6fe28b6a8678e7a7.jpeg',
+	'https://ichef.bbci.co.uk/images/ic/640x360/p01hc8c5.jpg',
+	'https://cruxnow.com/wp-content/uploads/2018/06/20180620T0829-0495-CNS-POPE-AUDIENCE-COMMANDS_800-1280x720.jpg',
+	'https://www.georgetown.edu/wp-content/uploads/2012/06/cara_web.jpg',
+	'https://www.catholicregister.org/media/k2/items/cache/06b838afa6933ac59c832da247bdc689_XL.jpg',
+	'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iJYCEXGkQLUw/v1/1000x-1.jpg',
+	'https://notesfrompoland.com/wp-content/uploads/2020/02/31740186517_ebddfbbfd9_k1.jpg',
+	'https://www.rcdea.org.uk/wp-content/uploads/2019/07/OrdinationiIveBishopAlanOutside750-750x480.jpg'
+];
+
+let aggrPriests = [
+	dir.concat('/aggrpriests/priest1.jpg'),
+	dir.concat('/aggrpriests/priest2.jpg'),
+	dir.concat('/aggrpriests/priest3.jpg'), 
+	dir.concat('/aggrpriests/priest4.jpg'), 
+	dir.concat('/aggrpriests/priest5.jpg'),
+	'https://limo.ismcdn.jp/mwimgs/f/3/-/img_f35b71b033e443326e03d99750fb411d505410.jpg',
+	'https://miro.medium.com/max/850/1*h7a9Iy3jxQWjfRVZMMFmlA.png',
+	'https://hcfmea.org/wp-content/uploads/2017/01/resolutions.jpg'
+];
+module.exports = { priestsarr, aggrPriests };
